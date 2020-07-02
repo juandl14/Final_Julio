d46a665e1823c22eae2ce7f0de4ee5885d8eaaf8
Juan DL <jdeluca@itba.edu.ar>
2020-07-02 12:36:26 -0300

    Commit inicial con los codigos fuente de la catedra

---
+++ src/Backend/CanvasState.java
@@ -0,0 +1,20 @@
+package Backend;
+
+import Backend.Model.Figure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CanvasState {
+
+    private final List<Figure> list = new ArrayList<>();
+
+    public void addFigure(Figure figure) {
+        list.add(figure);
+    }
+
+    public Iterable<Figure> figures() {
+        return new ArrayList<>(list);
+    }
+
+}
